1fc4d4e5044deebe6478c3fb61878cc75af7f7e5
Devil-Aura <imh203356@gmail.com>
2025-05-16 14:08:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,7 +46,7 @@ class Bot(Client):
 
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeflixSupport")
-        self.LOGGER(__name__).info(f"""BOT DEPLOYED BY @WORLD_FASTEST_BOTS"")
+        self.LOGGER(__name__).info(f"""BOT DEPLOYED BY @WORLD_FASTEST_BOTS""")
 
         self.set_parse_mode(ParseMode.HTML)
         self.username = usr_bot_me.username
